49b2867a3f52a74de9019312752c79a0f71768fc
Martyn van Dijke <martijnvdijke600@gmail.com>
2021-05-10 11:48:33 +0200

    update on line plots (now plots multiple lines like bar plot)

--- gr_lora_sdr_profiler/plotter.py
+++ gr_lora_sdr_profiler/plotter.py
@@ -70,22 +70,31 @@ class Plotter:
         _logger.debug("Making line plot")
         plot_x = prompt("Y value to choose ", completer=WordCompleter(self.colum_names)).strip()
         plot_y = prompt("X value to choose ", completer=WordCompleter(self.colum_names)).strip()
-        # plot_z = prompt("Value to groupby", completer=WordCompleter(self.colum_names)).strip()
+        plot_z = prompt("Value to groupby", completer=WordCompleter(self.colum_names)).strip()
+        agg = prompt("Aggregate by", completer=(WordCompleter(__aggregate_options__))).strip()
         # get labels for names
-        labels = get_config.get_label(plot_x, plot_y)
-        x_values = self.data_frame[plot_x]
-        y_values = self.data_frame[plot_y]
-        plt.plot(x_values, y_values)
+        labels = get_config.get_label(plot_x, plot_y, plot_z)
+
+        axes = self.data_frame.groupby([plot_x, plot_z])[plot_y].agg(agg).unstack().plot.line()
+        axes.legend(bbox_to_anchor=(1.04, 1), title=labels[2])
         plt.xlabel(labels[0])
         plt.ylabel(labels[1])
-        if self.output:
-            self.logger.debug("Writing plots to file")
-            filename = "/line_{0}_{1}".format(plot_x, plot_y)
-            plt.savefig(self.output_eps + filename + ".eps", format="eps")
-            plt.savefig(self.output_png + filename + ".png")
-        if self.show:
-            plt.show()
-        self.logger.debug("Line plotted %s vs %s", plot_x, plot_y)
+        # check if we want to plot all aggegrate options
+        agg_list = []
+        if agg == "all":
+            temp = __aggregate_options__.copy()
+            agg_list = temp[:-1]
+        else:
+            agg_list = [agg]
+        for agg in agg_list:
+            if self.output:
+                self.logger.debug("Writing plot to file")
+                filename = "/line_{0}_{1}_{2}_{3}".format(plot_x, plot_y, plot_z, agg)
+                plt.savefig(self.output_eps + filename + ".eps", format="eps")
+                plt.savefig(self.output_png + filename + ".png")
+            if self.show:
+                plt.show()
+            self.logger.debug("Line plotted %s vs %s", plot_x, plot_y)
 
     def bar_plot(self):
         """
@@ -104,14 +113,23 @@ class Plotter:
         axes.legend(bbox_to_anchor=(1.04, 1), title=labels[2])
         plt.xlabel(labels[0])
         plt.ylabel(labels[1])
-        if self.output:
-            self.logger.debug("Writing plots to file")
-            filename = "/bar_{0}_{1}_{2}_{3}".format(plot_x, plot_y, plot_z, agg)
-            plt.savefig(self.output_eps + filename + ".eps", format="eps", bbox_inches="tight")
-            plt.savefig(self.output_png + filename + ".png", bbox_inches="tight")
-        if self.show:
-            plt.show()
-        self.logger.debug("Bar plotted %s vs %s vs %s using %s", plot_x, plot_y, plot_z, agg)
+        # check if we want to plot all aggegrate options
+        agg_list = []
+        if agg == "all":
+            temp = __aggregate_options__.copy()
+            agg_list = temp[:-1]
+        else:
+            agg_list = [agg]
+        for agg in agg_list:
+            # start the plotting
+            if self.output:
+                self.logger.debug("Writing plot to file")
+                filename = "/bar_{0}_{1}_{2}_{3}".format(plot_x, plot_y, plot_z, agg)
+                plt.savefig(self.output_eps + filename + ".eps", format="eps", bbox_inches="tight")
+                plt.savefig(self.output_png + filename + ".png", bbox_inches="tight")
+            if self.show:
+                plt.show()
+            self.logger.debug("Bar plotted %s vs %s vs %s using %s", plot_x, plot_y, plot_z, agg)
 
     def barh_plot(self):
         """
@@ -145,7 +163,7 @@ class Plotter:
             plt.ylabel(labels[1])
             axes.legend(bbox_to_anchor=(1.04, 1), title=labels[2])
             if self.output:
-                self.logger.debug("Writing plots to file")
+                self.logger.debug("Writing plot to file")
                 filename = "/barh_{0}_{1}_{2}_{3}".format(plot_x, plot_y, plot_z, agg)
                 plt.savefig(self.output_eps + filename + ".eps", format="eps", bbox_inches="tight")
                 plt.savefig(self.output_png + filename + ".png", bbox_inches="tight")
